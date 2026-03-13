0251e074845291c06127e94b33bcdc3240acf77e
Manfrine <manfrine20@yahoo.com.br>
2026-03-13 16:43:41 -0400

    Limpando arquivos

--- aula02/Funcionario.java
+++
@@ -1,9 +0,0 @@
-package aula2;
-
-public class Funcionario {
-
-	String nome;
-	Double salario;
-	int idade;
-	String endereco;
-}
--- aula02/Gerente.java
+++
@@ -1,6 +0,0 @@
-package aula2;
-
-public class Gerente extends Funcionario {
-
-	Double comissao;
-}
